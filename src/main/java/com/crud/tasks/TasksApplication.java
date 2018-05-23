234f9786edf5f2592f400be6abffd34d2bf9e00a
Anna Czerska <anna.czerska@azetti.com>
2018-05-23 17:17:01 +0200

    REST API WEB app (#15.1 & #15.2)

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,9 +1,13 @@
 package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="com.crud.tasks", entityManagerFactoryRef="emf")
 public class TasksApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,23 +1,31 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
 
+    @Autowired
+    private DbService service;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
     @RequestMapping(
             method = RequestMethod.GET,
             value = "getTasks"
     )
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(
@@ -29,7 +37,7 @@ public class TaskController {
     }
 
     @RequestMapping(
-            method = RequestMethod.GET,
+            method = RequestMethod.DELETE,
             value = "deleteTask"
     )
     public void deleteTask(Long taskId) {
@@ -37,7 +45,7 @@ public class TaskController {
     }
 
     @RequestMapping(
-            method = RequestMethod.GET,
+            method = RequestMethod.PUT,
             value = "updateTask"
     )
     public TaskDto updateTask(TaskDto taskDto) {
@@ -45,7 +53,7 @@ public class TaskController {
     }
 
     @RequestMapping(
-            method = RequestMethod.GET,
+            method = RequestMethod.POST,
             value = "createTask"
     )
     public void createTask(TaskDto taskDto) {
