17a4f0a9ce91bbf0dee72c7a7a15955efe1c846c
Anna Czerska <ania.czerska@gmail.com>
2018-06-12 00:46:47 +0200

    Spring WEB REST - test coverage example (#21.2)

--- src/test/java/com/crud/tasks/trello/facade/TrelloMapperSuitTest.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloMapperSuitTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TrelloFacadeSuitTest {
+public class TrelloMapperSuitTest {
 
     @Autowired
     private TrelloMapper mapper;
